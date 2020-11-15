be86a6376f56a3db8902f7b96338a2d22fa32b5c
Zackwn <rafaelfavazzi@gmail.com>
2020-11-14 22:07:56 -0300

    find method

--- src/repositories/implementations/OrderRepository.ts
+++ src/repositories/implementations/OrderRepository.ts
@@ -15,11 +15,62 @@ export class OrderRepository implements IOrderRepository {
     })
   }
 
-  async find(options: FindOptions<Order>) {
+  async find(options?: FindOptions<Order>) {
     const db = await this.connection.connect()
-    // implement
+
+    /* 
+      where clause example
+     "WHERE name = $1 AND age = $2"
+    */
+
+    let whereClauseQuery = []
+    const whereClauseValues = []
+
+    if (options?.where) {
+      let index = 1
+
+      whereClauseQuery.push(' WHERE ')
+
+      for (let key in options.where) {
+        whereClauseQuery.push(`${key} = $${index}`, ' AND ')
+        whereClauseValues.push(options.where[key])
+        index++
+      }
+
+      whereClauseQuery.length = whereClauseQuery.length - 1
+    }
+
+    const selectedFiels = []
+
+    if (options?.select) {
+      options.select.forEach(field => {
+        selectedFiels.push(field, ', ')
+      })
+
+      selectedFiels.length = selectedFiels.length - 1
+    }
+
+    const finalQuery = [
+      'SELECT ',
+      `${selectedFiels.length >= 1
+        ? selectedFiels.join('')
+        : '* '
+      }`,
+      'FROM Orders',
+      `${whereClauseQuery.length >= 1
+        ? whereClauseQuery.join('')
+        : ''
+      }`
+    ].join('')
+
+    console.log({ finalQuery })
+
+    const orders = (await db.query<Order>(
+      finalQuery, whereClauseValues
+    )).rows
+
     db.release()
-    return [{} as Order]
+    return orders
   }
 
   async store(order: Order) {
