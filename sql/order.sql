5424cb230d4695797c19890636428eaa211c3b0c
Zackwn <rafaelfavazzi@gmail.com>
2020-11-16 15:12:19 -0300

    enum type for status

--- sql/order.sql
+++ sql/order.sql
@@ -1,6 +1,12 @@
+-- 1
 CREATE TABLE Orders (
     id varchar(36) PRIMARY KEY,
     "table" int,
     description TEXT,
     status varchar(255)
 );
+
+-- 2
+CREATE TYPE Status AS ENUM ('PENDING', 'DONE', 'CANCELED');
+ALTER TABLE Orders
+ALTER COLUMN status TYPE Status USING status::status;
