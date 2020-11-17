f4f6c98de0b64f6033672c7621d3bb1b3d35a99b
Zackwn <rafaelfavazzi@gmail.com>
2020-11-17 13:36:33 -0300

    separate server from app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import routes from './routes'
+import http from 'http'
+import { Server } from 'socket.io'
+import { SocketIO } from './providers/socket'
+
+const app = express()
+const server = http.createServer(app)
+
+const io = require('socket.io')(server)
+
+app.use((req, _, next) => {
+  const SocketIo = new SocketIO(io as Server)
+  req.socketIo = SocketIo
+  next()
+})
+app.use(express.json())
+app.use(routes)
+
+export { app, server }
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,4 @@
-import express from 'express'
-import routes from './routes'
-import http from 'http'
-import { Server } from 'socket.io'
-import { SocketIO } from './providers/socket'
-
-const app = express()
-const server = http.createServer(app)
-
-const io = require('socket.io')(server)
-
-app.use((req, _, next) => {
-  const SocketIo = new SocketIO(io as Server)
-  req.socketIo = SocketIo
-  next()
-})
-app.use(express.json())
-app.use(routes)
+import { server } from "./app"
 
 const PORT = 3333
 server.listen(PORT, () => console.log(PORT))
